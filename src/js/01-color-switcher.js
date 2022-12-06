07f335c31c56726e35c3ea6c2fdc3b0a027c9e0b
Halyna Kilbovych <vusochina.galina@gmail.com>
2022-12-06 14:44:45 +0200

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
 
+refs = { 
+    startBtn: document.querySelector("button[data-start]"),
+    stopBtn: document.querySelector("button[data-stop]"),
+};
+
+let timerId = null;
+
+refs.startBtn.addEventListener('click', onstartChangeColor); 
+refs.stopBtn.addEventListener('click', onstopChangeColor);
+
+function onstartChangeColor() { 
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();;
+      }, 1000);
+      refs.startBtn.disabled = true;
+      refs.stopBtn.disabled = false;
+};
+
+function onstopChangeColor() { 
+    clearInterval(timerId);
+    console.log(`Interval with id ${timerId} has stopped!`);
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
